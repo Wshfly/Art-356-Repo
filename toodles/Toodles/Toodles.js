26a8419fe723a61e956d6bb196e8ed928149e2ae
Wshfly <kcwhateley@gmail.com>
2026-09-01 17:26:35 -0700

    Fixeddestinationerror

--- toodles/Toodles/Toodles.js
+++ toodles/Toodles/Toodles.js
@@ -1,4 +1,4 @@
-let awnserResults = "no";
+let awnserResults = ["no", "yes", "maybe", "sometimes", "Ask Again Later", "Punch that mofo till he black n blue"]
 
 function setup() {
     createCanvas(500, 700);
@@ -6,12 +6,12 @@ function setup() {
 }
 
 function draw() {
-    background(220);
+    background(150);
 
     fill(0);
-    ellipse(width/2, height/2, 300, 300);
+    ellipse(width/2, 300, 300);
 
-    fill(255, 0, 0);
+    fill(255);
     textAlign(CENTER, CENTER);
     textSize(32);
     text("8", width/2, 300);
